4c6dffd7e1d22fc40faae320fceab70f68d06f29
Gabriel <gabrielsrgbi@gmail.com>
2020-04-01 18:13:22 -0300

    Modulo de cores RGB

---
+++ cores/cor.py
@@ -0,0 +1,173 @@
+Amarelo = (255, 255, 0)
+
+Amarelo_Esverdeado = (153, 204, 50)
+
+Aquamarine = (112, 219, 147)
+
+Aquamarine_Medio= (50, 205, 153)
+
+Azul = (0, 0, 255)
+
+Azul_Ardosia = (0, 127, 255)
+
+Azul_Ardosia_Escuro = (107, 35, 142)
+
+Azul_Brilhante = (192, 217, 217)
+
+Azul_Celeste = (50, 153, 204)
+
+Azul_Claro = (95, 159, 159)
+
+Azul_Corn_Flower = (66, 66, 111)
+
+Azul_Escuro = (0, 0, 156)
+
+Azul_Marinho = (35, 35, 142)
+
+Azul_Medio = (50, 50, 205)
+
+Azul_Neon = (77, 77, 255)
+
+Azul_Rich = (89, 89, 171)
+
+Azul_Violeta = (159, 95, 159)
+
+Bakers_Chocolate = (92, 51, 23)
+
+Branco = (255, 255, 255)
+
+Bright_Ouro = (217, 217, 25)
+
+Bronze = (219, 147, 112)
+
+Bronze_Claro = (235, 199, 158)
+
+Bronze_Escuro = (140, 120, 83)
+
+Caqui = (159, 95, 159)
+
+Cinza = (190, 190, 190)
+
+Cinza_Brilhante = (168, 168, 168)
+
+Cinza_Claro = (205, 205, 205)
+
+Cobre = (217, 135, 25)
+
+Cobre_Claro = (181, 165, 66)
+
+Cobre_Escuro = (184, 115, 51)
+
+Coral = (255, 127, 0)
+
+Cyan = (0, 255, 255)
+
+Escarlata = (140, 23, 23)
+
+Firebrick = (142, 35, 35)
+
+Goldenrod_Medio = (234, 234, 174)
+
+Goldenrod = (219, 219, 112)
+
+Laranja = (255, 165, 0)
+
+Laranja_Mandarian = (228, 120, 51)
+
+Light_Steel_Blue = (143, 143, 189)
+
+Light_Wood = (233, 194, 166)
+
+Magenta = (255, 0, 255)
+
+Marrom = (142, 35, 107)
+
+Marrom = (165, 42, 42)
+
+Marrom_Escuro = (92, 64, 51)
+
+Medium_Slate_Blue = (127, 0, 255)
+
+Orquidea_Escuro = (153, 50, 205)
+
+Orquidea_Medio = (147, 112, 219)
+
+Ouro = (205, 127, 50)
+
+Ouro_Velho = (207, 181, 59)
+
+Plum = (234, 173, 234)
+
+Preto = (0, 0, 0)
+
+Purpura_Escuro = (135, 31, 120)
+
+Quartz = (217, 217, 243)
+
+Rosa = (255, 192, 203)
+
+Rosa_Temperado = (255, 28, 174)
+
+Salmao = (111, 66, 66)
+
+Semi_Sweet_Chocolate = (107, 66, 38)
+
+Sienna = (142, 107, 35)
+
+Silver = (230, 232, 250)
+
+Steel_Azul = (35, 107, 142)
+
+Summer_Sky = (56, 176, 222)
+
+Tan_Escuro = (151, 105, 79)
+
+Thistle = (216, 191, 216)
+
+Turquesa = (173, 234, 234)
+
+Turquesa_Escuro = (112, 147, 219)
+
+Turquesa_Medio = (112, 219, 219)
+
+Verde = (0, 255, 0)
+
+Verde_Amarelado = (147, 219, 112)
+
+Verde_Cobre = (82, 127, 118)
+
+Verde_Cobre_Escuro = (74, 118, 110)
+
+Verde_Escuro = (47, 79, 47)
+
+Verde_Floresta = (35, 142, 35)
+
+Verde_Floresta_Medio = (107, 142, 35)
+
+Verde_Hunter = (33, 94, 33)
+
+Verde_Limao = (50, 205, 50)
+
+Verde_Oceano = (66, 111, 66)
+
+Verde_Oceano_claro = (35, 142, 104)
+
+Verde_Oliva_Escuro = (79, 79, 47)
+
+Verde_Primavera = (0, 255, 127)
+
+Verde_Primavera = (127, 255, 0)
+
+Vermelho = (255, 0, 0)
+
+Vermelho_Indiano = (78, 47, 47)
+
+Vermelho_Violeta = (204, 50, 153)
+
+Violeta = (79, 47, 79)
+
+Violeta_Vermelho_Medio = (219, 112, 147)
+
+Wheat = (216, 216, 191)
+
+Wood_Medio = (165, 128, 100)
