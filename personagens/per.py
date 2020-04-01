00df0254f9b27a20e6a02605eb90875e56d1eca0
Gabriel <gabrielsrgbi@gmail.com>
2020-04-01 18:12:16 -0300

    Modulo de personagens

---
+++ personagens/per.py
@@ -0,0 +1,29 @@
+import pygame
+
+jovem = [
+    [
+    pygame.image.load("img/pej_1.png"),
+    pygame.image.load("img/pej_2.png"),
+    pygame.image.load("img/pej_1.png"),
+    pygame.image.load("img/pej_3.png")
+
+     ],
+    [
+    pygame.image.load("img/pej_4.png"),
+    pygame.image.load("img/pej_5.png"),
+    pygame.image.load("img/pej_4.png"),
+    pygame.image.load("img/pej_6.png")
+     ],
+    [
+    pygame.image.load("img/pej_7.png"),
+    pygame.image.load("img/pej_8.png"),
+    pygame.image.load("img/pej_7.png"),
+    pygame.image.load("img/pej_9.png")
+    ],
+    [
+    pygame.image.load("img/pej_10.png"),
+    pygame.image.load("img/pej_11.png"),
+    pygame.image.load("img/pej_10.png"),
+    pygame.image.load("img/pej_12.png")
+    ]
+    ]
