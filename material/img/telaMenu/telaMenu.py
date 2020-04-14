633bd18d16ac6c57c3d7011b046052e7d31612a7
Gabriel <gabrielsrgbi@gmail.com>
2020-04-14 18:42:12 -0300

    criado

--- material/img/telaMenu/telaMenu.py
+++ material/img/telaMenu/telaMenu.py
@@ -1,2 +1,8 @@
 import pygame
 
+telaMenu = [
+    pygame.image.load("material/img/telaMenu/telaMenu0.png"),
+    pygame.image.load("material/img/telaMenu/telaMenu1.png"),
+    pygame.image.load("material/img/telaMenu/telaMenu2.png"),
+    pygame.image.load("material/img/telaMenu/telaMenu3.png")
+]
