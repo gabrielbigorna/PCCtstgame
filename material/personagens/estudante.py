be2d0c7ec43a203dcc3cbe6c64d51ec9206cb329
Gabriel <gabrielsrgbi@gmail.com>
2020-04-14 18:41:24 -0300

    alterado

---
+++ material/personagens/estudante.py
@@ -0,0 +1,29 @@
+import pygame
+
+estudante = [
+    [
+    pygame.image.load("material/img/estudante/estudante1.png"),
+    pygame.image.load("material/img/estudante/estudante2.png"),
+    pygame.image.load("material/img/estudante/estudante3.png"),
+    pygame.image.load("material/img/estudante/estudante4.png")
+
+     ],
+    [
+    pygame.image.load("material/img/estudante/estudante5.png"),
+    pygame.image.load("material/img/estudante/estudante6.png"),
+    pygame.image.load("material/img/estudante/estudante7.png"),
+    pygame.image.load("material/img/estudante/estudante8.png")
+     ],
+    [
+    pygame.image.load("material/img/estudante/estudante9.png"),
+    pygame.image.load("material/img/estudante/estudante10.png"),
+    pygame.image.load("material/img/estudante/estudante11.png"),
+    pygame.image.load("material/img/estudante/estudante12.png")
+    ],
+    [
+    pygame.image.load("material/img/estudante/estudante13.png"),
+    pygame.image.load("material/img/estudante/estudante14.png"),
+    pygame.image.load("material/img/estudante/estudante15.png"),
+    pygame.image.load("material/img/estudante/estudante16.png")
+    ]
+    ]
--- material/personagens/jovem.py
+++ material/personagens/jovem.py
@@ -2,28 +2,28 @@ import pygame
 
 jovem = [
     [
-    pygame.image.load("img/pej_1.png"),
-    pygame.image.load("img/pej_2.png"),
-    pygame.image.load("img/pej_1.png"),
-    pygame.image.load("img/pej_3.png")
+    pygame.image.load("material/img/pej/pej_1.png"),
+    pygame.image.load("material/img/pej/pej_2.png"),
+    pygame.image.load("material/img/pej/pej_1.png"),
+    pygame.image.load("material/img/pej/pej_3.png")
 
      ],
     [
-    pygame.image.load("img/pej_4.png"),
-    pygame.image.load("img/pej_5.png"),
-    pygame.image.load("img/pej_4.png"),
-    pygame.image.load("img/pej_6.png")
+    pygame.image.load("material/img/pej/pej_4.png"),
+    pygame.image.load("material/img/pej/pej_5.png"),
+    pygame.image.load("material/img/pej/pej_4.png"),
+    pygame.image.load("material/img/pej/pej_6.png")
      ],
     [
-    pygame.image.load("img/pej_7.png"),
-    pygame.image.load("img/pej_8.png"),
-    pygame.image.load("img/pej_7.png"),
-    pygame.image.load("img/pej_9.png")
+    pygame.image.load("material/img/pej/pej_7.png"),
+    pygame.image.load("material/img/pej/pej_8.png"),
+    pygame.image.load("material/img/pej/pej_7.png"),
+    pygame.image.load("material/img/pej/pej_9.png")
     ],
     [
-    pygame.image.load("img/pej_10.png"),
-    pygame.image.load("img/pej_11.png"),
-    pygame.image.load("img/pej_10.png"),
-    pygame.image.load("img/pej_12.png")
+    pygame.image.load("material/img/pej/pej_10.png"),
+    pygame.image.load("material/img/pej/pej_11.png"),
+    pygame.image.load("material/img/pej/pej_10.png"),
+    pygame.image.load("material/img/pej/pej_12.png")
     ]
     ]
